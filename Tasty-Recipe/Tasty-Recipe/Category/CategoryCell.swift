e74d219f94ca654bfbcb39c1d8bbf10a196b8016
shelly <shellyast123@gmail.com>
2021-07-01 05:44:24 -0700

    create table view for favorites and caterogies

---
+++ Tasty-Recipe/Tasty-Recipe/Category/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  Tasty-Recipe
+//
+//  Created by user196689 on 7/1/21.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    
+    	
+    @IBOutlet weak var categoryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
